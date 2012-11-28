d1dbbee58e222adb7b86b62d6985e30cb0a769ae
Brian Laub <blaub6@gatech.edu>
2012-11-27 23:05:05 -0500

    added installer scripts

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 [prefix]"
+    exit 1
+fi
+
+PREFIX=$1
+
+mkdir -p $PREFIX/bin $PREFIX/lib
+
+echo "generate scripts"
+cat > bin/monitor << EOF
+#!/bin/bash
+
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$PREFIX/lib
+$PREFIX/bin/basic_monitor \$*
+
+EOF
+
+cat > bin/collector << EOF
+#!/bin/bash
+
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$PREFIX/lib
+$PREFIX/bin/basic_receiver \$*
+
+EOF
+
+
+echo "install bin"
+install -m 755 bin/monitor $PREFIX/bin
+install -m 755 bin/collector $PREFIX/bin
+install -m 755 bin/basic_monitor $PREFIX/bin
+install -m 755 bin/basic_receiver $PREFIX/bin
+
+echo "install lib"
+cp -pdr lib/* $PREFIX/lib
---
+++ make_install_package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sandbox_dir=cloud_monitor-installer
+
+zmq_dir=external/zeromq-install
+msgpack_dir=external/msgpack-install
+mxml_dir=external/mxml-install
+
+set -x
+
+mkdir -p $sandbox_dir/bin
+mkdir -p $sandbox_dir/lib
+
+cp src/basic_monitor src/basic_receiver $sandbox_dir/bin
+
+cp -d $zmq_dir/lib/*.so* $zmq_dir/lib/*.la $sandbox_dir/lib
+cp -d $msgpack_dir/lib/*.so* $sandbox_dir/lib
+cp -d $mxml_dir/lib/*.so* $sandbox_dir/lib
+
+cp install.sh $sandbox_dir
+
